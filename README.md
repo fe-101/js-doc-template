915c2cb9eb4cffb2de085c622812bad312e08c53
halo <haloislet@gmail.com>
2018-02-08 13:51:37 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+JSDoc 模板
+===
+> 一个使用了 [Minami](https://github.com/Nijikokun/minami) 主题的 [JSDoc](https://github.com/jsdoc3/jsdoc) 项目模板。
+
---
+++ libs/is-sorted/index.js
@@ -0,0 +1,28 @@
+/**
+ * 检查数组排序的模块 {@link https://github.com/dcousens/is-sorted is-sorted GitHub Repo}.
+ * @module is-sorted 
+ */
+
+// 默认比较器
+function defaultComparator (a, b) {
+    return a - b
+  }
+  
+  
+  /**
+   * 检查数组排序
+   * @param {array} array - 需要检查的数组
+   * @param {function} [comparator] - 自定义比较器
+   * @return {boolean} 排序是否正确
+   */
+  
+  module.exports = function checksort (array, comparator) {
+    comparator = comparator || defaultComparator
+    
+    for (var i = 1, length = array.length; i < length; ++i) {
+      // 当上一个元素比下一个元素大的时候，数组不满足从小到大排序，返回 false
+      if (comparator(array[i - 1], array[i]) > 0) return false
+    }
+  
+    return true
+  }
